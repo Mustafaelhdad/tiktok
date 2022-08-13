19cf37130a2ec8348a204009b81c7919a5a98f5e
Mustafaelhdad <Mostafa.eng_2194@eng.kfs.edu.eg>
2022-08-13 23:56:47 +0200

    changed the data for a static data for testing

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -12,6 +12,10 @@ interface IProps {
 }
 
 const VideoCard: NextPage<IProps> = ({ post }) => {
+  const [isHover, setIsHover] = useState(false);
+  const [playing, setPlaying] = useState(false);
+  const [isVideoMuted, setIsVideoMuted] = useState(false);
+
   return (
     <div className="flex flex-col border-b-2 border-gray-200 pb-6">
       <div>
@@ -23,7 +27,8 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
                   width={62}
                   height={62}
                   className="rounded-full"
-                  src={post.postedBy.image}
+                  // src={post.postedBy.image}
+                  src="/components/original.jpg"
                   alt="user-profile"
                   layout="responsive"
                 />
@@ -34,14 +39,55 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
         <Link href="/">
           <div className="flex items-center gap-2">
             <p className="flex gap-2 items-center md:text-md font-bold text-primary">
-              {post.postedBy.userName} {` `}
+              {/* {post.postedBy.userName} {` `} */}
+              {"Mustafa"} {` `}
               <GoVerified className="text-blue-400 text-md" />
             </p>
             <p className="capitalize font-medium text-xs text-gray-500 hidden md:block">
-              {post.postedBy.userName}
+              {/* {post.postedBy.userName} */}
+              {"Mustafa"}
             </p>
           </div>
         </Link>
+      </div>
+      <div className="lg:ml-20 flex gap-4 relative">
+        <div
+          onMouseEnter={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+          className="rounded-3xl"
+        >
+          <Link href="/">
+            <video
+              loop
+              className="lg:w[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
+              // src={post.video.asset.url}
+              src="/components/pain.MP4"
+            ></video>
+          </Link>
+
+          {isHover && (
+            <div>
+              {playing ? (
+                <button>
+                  <BsFillPauseFill className="text-black text-2xl lg:text-4xl" />
+                </button>
+              ) : (
+                <button>
+                  <BsFillPlayFill className="text-black text-2xl lg:text-4xl" />
+                </button>
+              )}
+              {isVideoMuted ? (
+                <button>
+                  <HiVolumeOff className="text-black text-2xl lg:text-4xl" />
+                </button>
+              ) : (
+                <button>
+                  <HiVolumeUp className="text-black text-2xl lg:text-4xl" />
+                </button>
+              )}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
