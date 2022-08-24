6de10b61877f83caceab7ac15cdd328f0c741fa9
Mustafaelhdad <Mostafa.eng_2194@eng.kfs.edu.eg>
2022-08-24 11:10:16 +0200

    small fixes

--- components/SuggestedAccounts.tsx
+++ components/SuggestedAccounts.tsx
@@ -1,48 +1,42 @@
 import React, { useEffect } from "react"
 import Image from "next/image"
-import { NextPage } from "next"
 import Link from "next/link"
 import { GoVerified } from "react-icons/go"
 
+import useAuthStore from "../store/authStore"
 import { IUser } from "../types"
 
-interface IProps {
-  fetchAllUsers: () => void
-  allUsers: IUser[]
-}
+const SuggestedAccounts = () => {
+  const { fetchAllUsers, allUsers } = useAuthStore()
 
-const SuggestedAccounts: NextPage<IProps> = ({ fetchAllUsers, allUsers }) => {
   useEffect(() => {
     fetchAllUsers()
   }, [fetchAllUsers])
 
-  const users = allUsers
-    .sort(() => 0.5 - Math.random())
-    .slice(0, allUsers.length)
-
   return (
     <div className="xl:border-b-2 border-gray-200 pb-4">
       <p className="text-gray-500 font-semibold m-3 mt-4 hidden xl:block">
-        Suggested accounts
+        Suggested Accounts
       </p>
+
       <div>
-        {users?.slice(0, 6).map((user: IUser) => (
+        {allUsers.slice(0, 6).map((user: IUser) => (
           <Link href={`/profile/${user._id}`} key={user._id}>
             <div className="flex gap-3 hover:bg-primary p-2 cursor-pointer font-semibold rounded">
               <div className="w-8 h-8">
                 <Image
+                  src={user.image}
                   width={34}
                   height={34}
                   className="rounded-full"
-                  src={user.image}
-                  alt="user-profile"
+                  alt="user profile"
                   layout="responsive"
                 />
               </div>
 
               <div className="hidden xl:block">
                 <p className="flex gap-1 items-center text-md font-bold text-primary lowercase">
-                  {user.userName.replace(/\s+/g, "")}{" "}
+                  {user.userName.replaceAll(" ", "")}
                   <GoVerified className="text-blue-400" />
                 </p>
                 <p className="capitalize text-gray-400 text-xs">
